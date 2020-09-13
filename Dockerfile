e98fb26e6b9f8248385069e4ad40c6034ffb0f21
Parchuk <sergeyparchuk1@gmail.com>
2020-09-13 18:09:05 +0300

    added credentials for database

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,7 @@ MAINTAINER BootShop
 WORKDIR /var/www
 COPY . /var/www
 
+RUN touch /var/www/helper/config.js
 RUN npm install
 
 ENTRYPOINT ["npm", "start"]
--- helper/database.js
+++ helper/database.js
@@ -1,7 +1,10 @@
 const Sequalize = require("sequelize");
+const { password, username } = undefined || require('./config.js');
+const USERNAME = process.env.DB_ACCESS_KEY_ID || username;
+const PASSWORD = process.env.DB_SECRET_ACCESS_KEY || password;
 
-const sequalize = new Sequalize("bootshop", "frontend-94", "*frontend*", {
+const sequalize = new Sequalize('bootshop', USERNAME, PASSWORD, {
     dialect: "mysql",
-    host: "172.17.0.3",
+    host: '51.116.182.16',
 });
 module.exports = sequalize;
