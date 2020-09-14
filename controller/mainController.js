5668647b81e3577cf4984ecacaf50322c1487468
Parchuk <sergeyparchuk1@gmail.com>
2020-09-14 19:56:07 +0300

    added rout for page product_details and fixed style for img on page index

--- controller/mainController.js
+++ controller/mainController.js
@@ -2,56 +2,67 @@ const Product = require("../models/product");
 
 exports.index = (req, res, next) => {
     Product.findAll()
-    .then((products) => {
-      res.render("pages/index", {
-        products: products,
-        pageTitle: "All products",
-        path: 'pages/index'
-      });
-    })
-    .catch(err => console.log(err))
-}
+        .then((products) => {
+            res.render("pages/index", {
+                products: products,
+                pageTitle: "All products",
+                path: 'pages/index'
+            });
+        })
+        .catch(err => console.log(err));
+};
 exports.faq = (req, res, next) => {
-    res.render("pages/faq")
-}
+    res.render("pages/faq");
+};
 exports.contact = (req, res, next) => {
-    res.render("pages/contact")
-}
+    res.render("pages/contact");
+};
 exports.delivery = (req, res, next) => {
-    res.render("pages/delivery")
-}
+    res.render("pages/delivery");
+};
 exports.forgetpass = (req, res, next) => {
-    res.render("pages/forgetpass")
-}
+    res.render("pages/forgetpass");
+};
 exports.legal_notice = (req, res, next) => {
-    res.render("pages/legal_notice")
-}
+    res.render("pages/legal_notice");
+};
 exports.login = (req, res, next) => {
-    res.render("pages/login")
-}
+    res.render("pages/login");
+};
 exports.product_details = (req, res, next) => {
-    res.render("pages/product_details")
-}
+    const id = req.params.id;
+    Product.findAll({
+        where: {
+            id: id
+        }
+    }).then(products => {
+        res.render("pages/product_details", {
+            products: products,
+            pageTitle: "Product details",
+            // path: '/product_details'
+        });
+    }).catch(err => console.log(err));
+};
 exports.product_summary = (req, res, next) => {
-    res.render("pages/product_summary")
-}
+    res.render("pages/product_summary");
+};
 exports.products = (req, res, next) => {
     Product.findAll()
-    .then((products) => {
-      res.render("pages/products", {
-        products: products,
-        pageTitle: "All products",
-        path: 'pages/products'
-      });
-    })
-    .catch(err => console.log(err))
-}
+        .then((products) => {
+            res.render("pages/products", {
+                products: products,
+                pageTitle: "All products",
+                path: 'pages/products'
+            });
+        })
+        .catch(err => console.log(err));
+};
 exports.register = (req, res, next) => {
-    res.render("pages/register")
-}
+    res.render("pages/register");
+};
 exports.special_offer = (req, res, next) => {
-    res.render("pages/special_offer")
-}
+    res.render("pages/special_offer");
+};
 exports.tac = (req, res, next) => {
-    res.render("pages/tac")
-}
+    res.render("pages/tac");
+};
