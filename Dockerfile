131ad4f9398b22904d5065963bd40d94885ad71d
Parchuk <sergeyparchuk1@gmail.com>
2020-09-16 13:50:17 +0300

    recorected file database, gitignore and Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,6 @@ MAINTAINER BootShop
 WORKDIR /var/www
 COPY . /var/www
 
-RUN touch /var/www/helper/config.js
 RUN npm install
 
 ENTRYPOINT ["npm", "start"]
--- helper/database.js
+++ helper/database.js
@@ -1,7 +1,6 @@
 const Sequalize = require("sequelize");
-const { password, username } = undefined || require('./config.js');
-const USERNAME = process.env.DB_ACCESS_KEY_ID || username;
-const PASSWORD = process.env.DB_SECRET_ACCESS_KEY || password;
+const USERNAME = process.env.DB_ACCESS_KEY_ID;
+const PASSWORD = process.env.DB_SECRET_ACCESS_KEY;
 
 const sequalize = new Sequalize('bootshop', USERNAME, PASSWORD, {
     dialect: "mysql",
