d89042b83ce8da0f044233f5bc5f6aaf71ce0980
Parchuk <sergeyparchuk1@gmail.com>
2020-09-30 19:34:17 +0300

    added cart

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ const sequalize = require("./helper/database");
 
 const User = require("./models/users");
 const Product = require("./models/product");
+const Cart = require("./models/cart");
+const CartItem = require("./models/cartItem");
+
+// seed
 const seedProducts = require('./seedProducts');
 
 // middleware
@@ -25,9 +29,26 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "static")));
 app.use(['/admin/products_edit/', '/product_details/:id', '/admin/'], express.static(path.join(__dirname, 'static')));
 
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
 app.use(mainRoutes);
 app.use(adminRoutes);
 app.use(errorController.get404);
+
+// Relations
+Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
+User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequalize
   .sync()
@@ -47,7 +68,9 @@ sequalize
         });
       }
       return user;
-    }).catch(err => console.log(err));
+    }).then((user) => {
+      return user.createCart();
+    });
   })
   .then((result) => {
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
